61f5e4fe4af53d0886c6118108fd25a9327fd4b0
furti <daniel.furtlehner@gmx.net>
2020-01-04 10:42:33 +0100

    Add Section Plane commands to toolbar

--- gui/toolbar_manager.py
+++ gui/toolbar_manager.py
@@ -13,3 +13,5 @@ toolbarManager = ToolbarManager()
 # import commands here
 import commands.create_floor_builder
 import commands.create_wood_extract
+import commands.create_section_plane
+import commands.export_section_svg
